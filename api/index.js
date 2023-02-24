0f7bdad21802bc131ba097b9e65c8b85ac780d65
Nikodem Graczewski <ngraczewski.sda@gmail.com>
2023-02-24 22:42:41 +0100

    added login endpoint

--- api/index.js
+++ api/index.js
@@ -1,16 +1,21 @@
 const app = require("express")();
-const { v4 } = require("uuid");
 
-app.get("/api", (req, res) => {
-  const path = `/api/item/${v4()}`;
-  res.setHeader("Content-Type", "text/html");
-  res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`);
-});
+const users = [
+  { username: "user1", password: "abcd1234" },
+  { username: "user2", password: "1234abcd" },
+];
+app.get("/api/login", (req, res) => {
+  const { username, password } = req.body;
 
-app.get("/api/item/:slug", (req, res) => {
-  const { slug } = req.params;
-  res.end(`Item: ${slug}`);
+  const validCredentials = users.some((user) => {
+    return user.password === password && user.username === username;
+  });
+
+  if (validCredentials) {
+    res.end("Login successful");
+  }
+
+  res.status(401);
 });
 
 module.exports = app;
